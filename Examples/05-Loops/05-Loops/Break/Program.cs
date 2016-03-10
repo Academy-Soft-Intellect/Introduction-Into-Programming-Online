e9e4dc28efce2adad870eef21a8b4e627cab1d44
Babalev <ivaylo.babalev@gmail.com>
2016-03-10 18:39:44 +0200

    Loops

---
+++ Examples/05-Loops/05-Loops/Break/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            //int number = 3;
+            //int divider = 5;
+            //int curent_number = 1;
+
+            //do
+            //{
+            //    int result = number * curent_number;
+            //    curent_number++;
+
+            //    if (result % divider == 0)
+            //    {
+            //        Console.Write("Least common multiple (LCM): {0} and {1} is {2}", number, divider, result);
+            //        Console.WriteLine();
+            //        break;
+            //    }
+
+            //} while (true);
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+            int sum = 0;
+
+            while (true)
+            {
+                int i = int.Parse(Console.ReadLine());
+                if (i == 0)
+                {
+                    break;
+                }
+                sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Examples/05-Loops/05-Loops/WhileLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            while (counter <= 10)
+            {
+                if (counter % 2 == 0)
+                {
+                    Console.WriteLine(counter);
+                }
+                counter++;
+            }
+            Console.WriteLine("Loop ended!");
+        }
+    }
+}
