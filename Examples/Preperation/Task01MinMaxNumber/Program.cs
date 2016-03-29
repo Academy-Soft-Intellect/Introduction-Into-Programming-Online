76632318fb432433335e8c40d1dbe0ce5ef9a75a
babalev <ivaylo.babalev@gmail.com>
2016-03-29 19:27:15 +0300

    exampl

---
+++ Examples/Preperation/Task01MinMaxNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01MinMaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = int.MaxValue;
+            int maxNumber = int.MinValue;
+
+            for (int i = 0; i < 5; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+                if (number < minNumber)
+                {
+                    minNumber = number;
+                }
+            }
+
+            Console.WriteLine("Max number is:" + maxNumber);
+            Console.WriteLine("Min number is:" + minNumber);
+        }
+    }
+}
---
+++ Examples/Preperation/Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
