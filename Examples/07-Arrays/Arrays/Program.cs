7a463273de49925df8abb476f0640aecba75d3d5
babalev <ivaylo.babalev@gmail.com>
2016-04-02 13:50:30 +0300

    The exam task

--- Examples/07-Arrays/Arrays/Program.cs
+++ Examples/07-Arrays/Arrays/Program.cs
@@ -10,13 +10,51 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            int[,] arr = new int[10, 10];
+            Console.WriteLine("Please enter array length:");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            int sum = 0;
 
-            int[,] arr2 = new int[,] { { 1, 2 }, { 1, 2 }, { 1, 2 } };
+            for (int i = 0; i < array.Length; i++)
 
-            int[] arr3 = new int[5];
+            {
+                Console.WriteLine("Please enter a number for the element at index " + i);
+                array[i] = int.Parse(Console.ReadLine());
+            }
 
-            Console.WriteLine(arr3.Length);
+            foreach (var item in array)
+            {
+                Console.Write(" |" + item + "| ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            {
+                Console.WriteLine(sum);
+            }
+            int side = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[side, side];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    if (side >= 2 && side < 10)
+                    {
+                        matrix[i, j] = array[i];
+                    }
+            }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("|" + " " + matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ Examples/Exam/Exam/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int side = 0;
+            do
+            {
+                Console.WriteLine("Please enter matrix side, between 2 and 9:");
+                side = int.Parse(Console.ReadLine());
+            } while (side > 9 || side < 2);
+
+            int[] array = new int[side * side];
+
+            int[,] matrix = new int[side, side];
+
+            int sum = 0;
+
+            Console.WriteLine("Please fill in the array");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Enter element at index {0}", i);
+                array[i] = int.Parse(Console.ReadLine());
+                sum += array[i];
+            }
+
+            int counter = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[j, i] = array[counter];
+                    counter++;
+                }
+            }
+
+
+            Console.WriteLine("The matrix looks like:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("The sum of the elements in the matrix is: {0}", sum);
+        }
+    }
+}
